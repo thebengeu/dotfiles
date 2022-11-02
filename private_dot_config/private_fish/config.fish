fe32f4d7cdc749a135f2d182c5809271c4141d9d
Beng Eu <beng@bengeu.com>
2022-11-02 12:43:38 +0800

    Add McFly and Starship

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,4 +1,7 @@
-if status is-interactive
-end
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+fish_add_path ~/.cargo/bin
 fish_add_path ~/go/bin
+if status is-interactive
+  mcfly init fish | source
+  starship init fish | source
+end
