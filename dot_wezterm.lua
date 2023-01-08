88e8e667406eba37b31dfe3ae12dabcd1bbc147a
Beng Eu <beng@bengeu.com>
2023-01-08 22:40:49 +0800

    Change wezterm font size to 11 points

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,5 +2,6 @@ local wezterm = require 'wezterm'
 return {
   color_scheme = 'Catppuccin Mocha',
   font = wezterm.font 'MonoLisa Variable',
+  font_size = 11,
   hide_tab_bar_if_only_one_tab = true
 }
