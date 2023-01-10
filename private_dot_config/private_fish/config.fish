d0352d62797572d43027ae17122dab869dbb70f3
Beng Eu <beng@bengeu.com>
2023-01-10 14:03:33 +0800

    Add broot fish function

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -30,6 +30,18 @@ if status is-interactive
     abbr --add pr 'gh pr create -f'
     abbr --add prr 'gh pr create -f -r'
     abbr --add vim nvim
+    function br --wraps=broot
+        set -l cmd_file (mktemp)
+        if broot --outcmd $cmd_file $argv
+            read --local --null cmd < $cmd_file
+            rm -f $cmd_file
+            eval $cmd
+        else
+            set -l code $status
+            rm -f $cmd_file
+            return $code
+        end
+    end
     function fish_title
         if test -n "$TMUX"
             tmux rename-window -t $(tmux display-message -p '#{window_index}') $PWD
