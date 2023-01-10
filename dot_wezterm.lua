6f4201f3c682bd86f10a324364d548c5abdaa87d
Beng Eu <beng@bengeu.com>
2023-01-10 18:47:30 +0800

    Use MonoLisa script feature for italic in WezTerm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -3,6 +3,16 @@ return {
   color_scheme = 'Catppuccin Mocha',
   default_prog = { 'tmux', 'new-session', '-A', '-s', '0' },
   font = wezterm.font 'MonoLisa Variable',
+  font_rules = {
+    {
+      font = wezterm.font {
+        family = 'MonoLisa Variable',
+        harfbuzz_features = { 'ss02' },
+        style = 'Italic',
+      },
+      italic = true
+    },
+  },
   font_size = 11,
   hide_tab_bar_if_only_one_tab = true
 }
