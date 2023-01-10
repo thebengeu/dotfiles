31827697684842e7d269676724fdbeab90129137
Beng Eu <beng@bengeu.com>
2023-01-10 16:13:26 +0800

    Start tmux by default for WezTerm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
 return {
   color_scheme = 'Catppuccin Mocha',
+  default_prog = { 'tmux', 'new-session', '-A', '-s', '0' },
   font = wezterm.font 'MonoLisa Variable',
   font_size = 11,
   hide_tab_bar_if_only_one_tab = true
