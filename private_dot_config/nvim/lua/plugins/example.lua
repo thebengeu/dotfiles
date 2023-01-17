6a5cbb3013f4183e84eabbd757884cfc1c39e11f
Beng Eu <beng@bengeu.com>
2023-01-17 18:19:45 +0800

    Add plugins and set color scheme for LazyVim

--- private_dot_config/nvim/lua/plugins/example.lua
+++ private_dot_config/nvim/lua/plugins/example.lua
@@ -1,6 +1,16 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then return {
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "catppuccin",
+      },
+    },
+    { "christoomey/vim-tmux-navigator" },
+    { "wakatime/vim-wakatime" },
+  }
+end
 
 -- every spec file under config.plugins will be loaded automatically by lazy.nvim
 --
