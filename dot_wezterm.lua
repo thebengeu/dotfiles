2616ebb3163567293c9ed5188251b4ea004bae81
Beng Eu <beng@bengeu.com>
2023-01-31 04:06:06 +0800

    Add WSL config for Wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,18 +1,20 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 return {
-  color_scheme = 'Catppuccin Mocha',
-  default_prog = { 'tmux', 'new-session', '-A', '-s', '0' },
-  font = wezterm.font 'MonoLisa Variable',
-  font_rules = {
-    {
-      font = wezterm.font {
-        family = 'MonoLisa Variable',
-        harfbuzz_features = { 'ss02' },
-        style = 'Italic',
-      },
-      italic = true
-    },
-  },
-  font_size = 11,
-  hide_tab_bar_if_only_one_tab = true
+	color_scheme = "Catppuccin Mocha",
+	-- default_domain = "WSL:Ubuntu",
+	default_prog = { "tmux", "new-session", "-A", "-s", "0" },
+	font = wezterm.font("MonoLisa Variable"),
+	font_rules = {
+		{
+			font = wezterm.font({
+				family = "MonoLisa Variable",
+				harfbuzz_features = { "ss02" },
+				style = "Italic",
+			}),
+			italic = true,
+		},
+	},
+	font_size = 11,
+	hide_tab_bar_if_only_one_tab = true,
+	skip_close_confirmation_for_processes_named = { "conhost.exe", "wsl.exe", "wslhost.exe" },
 }
