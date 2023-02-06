c2277e1a4162d7256bdb7f3b499455d006f1f565
Beng Eu <beng@bengeu.com>
2023-02-06 20:09:07 +0800

    Format config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,70 +4,70 @@ fish_add_path ~/.local/bin
 fish_add_path ~/doomemacs/bin
 fish_add_path ~/go/bin
 if status is-interactive
-  set --export --global BAT_PAGER 'less --mouse --quit-if-one-screen --RAW-CONTROL-CHARS'
-  set --export --global EDITOR nvim
-  set --export --global MCFLY_DELETE_WITHOUT_CONFIRM TRUE
-  set --export --global MCFLY_DISABLE_MENU TRUE
-  set --export --global MCFLY_KEY_SCHEME vim
-  set --global fish_greeting
-  fnm env --use-on-cd | source
-  mcfly init fish | source
-  zoxide init --cmd cd fish | source
-  abbr --add b bat
-  abbr --add bi 'brew install'
-  abbr --add bui 'brew uninstall'
-  abbr --add ca 'chezmoi apply'
-  abbr --add cgca 'chezmoi git -- commit --amend'
-  abbr --add cgcam 'chezmoi git -- commit -a -m'
-  abbr --add cgd 'chezmoi git diff'
-  abbr --add cgP 'chezmoi git push'
-  abbr --add cgp 'chezmoi git pull'
-  abbr --add cgs 'chezmoi git status'
-  abbr --add cgss 'chezmoi git -- status -s'
-  abbr --add clg 'lazygit --path ~/.local/share/chezmoi'
-  abbr --add cr 'chezmoi re-add'
-  abbr --add cat bat
-  abbr --add e 'emacsclient -nw'
-  abbr --add g git
-  abbr --add gaa 'git add -A'
-  abbr --add gca 'git commit --amend'
-  abbr --add gcam 'git commit -a -m'
-  abbr --add gcm 'git commit -m'
-  abbr --add gco 'git checkout'
-  abbr --add gd 'git diff'
-  abbr --add gl 'git log'
-  abbr --add gP 'git push'
-  abbr --add gp 'git pull'
-  abbr --add grbc 'git rebase --continue'
-  abbr --add gs 'git status'
-  abbr --add gss 'git status -s'
-  abbr --add lg 'lazygit'
-  abbr --add ll 'exa --git --icons --long'
-  abbr --add ls 'exa --icons'
-  abbr --add n nvim
-  abbr --add pr 'gh pr create -f'
-  abbr --add prr 'gh pr create -f -r'
-  abbr --add vim nvim
-  function br --wraps=broot
-      set -l cmd_file (mktemp)
-      if broot --outcmd $cmd_file $argv
-          read --local --null cmd < $cmd_file
-          rm -f $cmd_file
-          eval $cmd
-      else
-          set -l code $status
-          rm -f $cmd_file
-          return $code
-      end
-  end
-  function fish_title
-      if test -n "$TMUX"
-          tmux rename-window -t $(tmux display-message -p '#{window_index}') $PWD
-      end
-      pwd
-  end
-  function fish_user_key_bindings
-      fish_default_key_bindings -M insert
-      fish_vi_key_bindings --no-erase insert
-  end
+    set --export --global BAT_PAGER 'less --mouse --quit-if-one-screen --RAW-CONTROL-CHARS'
+    set --export --global EDITOR nvim
+    set --export --global MCFLY_DELETE_WITHOUT_CONFIRM TRUE
+    set --export --global MCFLY_DISABLE_MENU TRUE
+    set --export --global MCFLY_KEY_SCHEME vim
+    set --global fish_greeting
+    fnm env --use-on-cd | source
+    mcfly init fish | source
+    zoxide init --cmd cd fish | source
+    abbr --add b bat
+    abbr --add bi 'brew install'
+    abbr --add bui 'brew uninstall'
+    abbr --add ca 'chezmoi apply'
+    abbr --add cgca 'chezmoi git -- commit --amend'
+    abbr --add cgcam 'chezmoi git -- commit -a -m'
+    abbr --add cgd 'chezmoi git diff'
+    abbr --add cgP 'chezmoi git push'
+    abbr --add cgp 'chezmoi git pull'
+    abbr --add cgs 'chezmoi git status'
+    abbr --add cgss 'chezmoi git -- status -s'
+    abbr --add clg 'lazygit --path ~/.local/share/chezmoi'
+    abbr --add cr 'chezmoi re-add'
+    abbr --add cat bat
+    abbr --add e 'emacsclient -nw'
+    abbr --add g git
+    abbr --add gaa 'git add -A'
+    abbr --add gca 'git commit --amend'
+    abbr --add gcam 'git commit -a -m'
+    abbr --add gcm 'git commit -m'
+    abbr --add gco 'git checkout'
+    abbr --add gd 'git diff'
+    abbr --add gl 'git log'
+    abbr --add gP 'git push'
+    abbr --add gp 'git pull'
+    abbr --add grbc 'git rebase --continue'
+    abbr --add gs 'git status'
+    abbr --add gss 'git status -s'
+    abbr --add lg lazygit
+    abbr --add ll 'exa --git --icons --long'
+    abbr --add ls 'exa --icons'
+    abbr --add n nvim
+    abbr --add pr 'gh pr create -f'
+    abbr --add prr 'gh pr create -f -r'
+    abbr --add vim nvim
+    function br --wraps=broot
+        set -l cmd_file (mktemp)
+        if broot --outcmd $cmd_file $argv
+            read --local --null cmd <$cmd_file
+            rm -f $cmd_file
+            eval $cmd
+        else
+            set -l code $status
+            rm -f $cmd_file
+            return $code
+        end
+    end
+    function fish_title
+        if test -n "$TMUX"
+            tmux rename-window -t $(tmux display-message -p '#{window_index}') $PWD
+        end
+        pwd
+    end
+    function fish_user_key_bindings
+        fish_default_key_bindings -M insert
+        fish_vi_key_bindings --no-erase insert
+    end
 end
