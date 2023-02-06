47740e3909a7d1045e6f12840fe25734a2f4dd4c
Beng Eu <beng@bengeu.com>
2023-02-07 00:51:26 +0800

    Add "Find Plugin Files"

--- private_dot_config/nvim/lua/plugins/plugins.lua
+++ private_dot_config/nvim/lua/plugins/plugins.lua
@@ -742,6 +742,15 @@ return {
     },
     keys = {
       { "<leader><space>", false },
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({
+            cwd = require("lazy.core.config").options.root,
+          })
+        end,
+        desc = "Find Plugin Files",
+      },
     },
     opts = {
       defaults = {
