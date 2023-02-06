743948458c6fed7602b70d40d8faf345dff2ae88
Beng Eu <beng@bengeu.com>
2023-02-06 15:42:16 +0800

    Add tabout.nvim

--- private_dot_config/nvim/lua/plugins/plugins.lua
+++ private_dot_config/nvim/lua/plugins/plugins.lua
@@ -289,14 +289,7 @@ return {
         end,
       },
     },
-    ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
       local cmp = require("cmp")
       local luasnip = require("luasnip")
 
@@ -310,8 +303,6 @@ return {
             cmp.select_next_item()
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
           else
             fallback()
           end
@@ -767,6 +758,12 @@ return {
     keys = {
       { "<leader>cs", "<Cmd>SymbolsOutline<CR>", desc = "Symbols Outline" },
     },
+  },
+  {
+    "abecodes/tabout.nvim",
+    config = true,
+    dependencies = { "hrsh7th/nvim-cmp" },
+    event = "InsertEnter",
   },
   {
     "nvim-telescope/telescope.nvim",
