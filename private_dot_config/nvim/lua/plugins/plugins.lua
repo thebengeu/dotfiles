06999bf621eb99fbc3912b9070d7e9e593d83ac1
Beng Eu <beng@bengeu.com>
2023-02-08 23:10:53 +0800

    Restore <c-k> for "Signature Help" for consistency since it's used for tmux instead of kill line in shell

--- private_dot_config/nvim/lua/plugins/plugins.lua
+++ private_dot_config/nvim/lua/plugins/plugins.lua
@@ -431,13 +431,10 @@ return {
     "neovim/nvim-lspconfig",
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "<c-k>", false, mode = "i" }
       keys[#keys + 1] = { "gd", false }
       keys[#keys + 1] = { "gI", false }
       keys[#keys + 1] = { "gr", false }
       keys[#keys + 1] = { "gt", false }
-      keys[#keys + 1] =
-        { "<C-l>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "signatureHelp" }
     end,
     opts = {
       capabilities = {
@@ -619,13 +616,6 @@ return {
         "<C-e>",
         function()
           require("readline").end_of_line()
-        end,
-        mode = "!",
-      },
-      {
-        "<C-k>",
-        function()
-          require("readline").kill_line()
         end,
         mode = "!",
       },
