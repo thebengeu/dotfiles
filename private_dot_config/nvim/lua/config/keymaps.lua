5fe56d3e0537cfa64aa65d49279f82e56a52960e
Beng Eu <beng@bengeu.com>
2023-02-10 16:16:13 +0800

    Fix case when commit message has more than one word

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,6 @@
-vim.keymap.set("n", "<leader>gC", "<Cmd>exec ':!git commit -a -m ' . input('')<CR>", { desc = "Git commit all" })
+vim.keymap.set(
+  "n",
+  "<leader>gC",
+  [[<Cmd>exec ":!git commit -a -m '" . input("") . "'"<CR>]],
+  { desc = "Git commit all" }
+)
