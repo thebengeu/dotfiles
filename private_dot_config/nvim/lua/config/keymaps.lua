8b47f052f929aa251528f0e4736ed9acaa679a39
Beng Eu <beng@bengeu.com>
2023-02-12 01:27:04 +0800

    Update file before git commit

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,12 @@
 vim.keymap.set(
   "n",
   "<leader>ga",
-  [[<Cmd>exec ":!git commit -a -m '" . input("") . "'" | AsyncRun -mode=term -pos=hide git push<CR>]],
+  [[<Cmd>update | exec ":!git commit -a -m '" . input("") . "'" | AsyncRun -mode=term -pos=hide git push<CR>]],
   { desc = "Git commit all" }
 )
 vim.keymap.set(
   "n",
   "<leader>gC",
-  [[<Cmd>exec ":!git commit -m '" . input("") . "'" | AsyncRun -mode=term -pos=hide git push<CR>]],
+  [[<Cmd>update | exec ":!git commit -m '" . input("") . "'" | AsyncRun -mode=term -pos=hide git push<CR>]],
   { desc = "Git commit" }
 )
