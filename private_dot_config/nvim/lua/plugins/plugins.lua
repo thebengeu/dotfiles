a2a6cef9b5028098ddfa19e8a1e643797ad4c1a6
Beng Eu <beng@bengeu.com>
2023-02-12 23:05:14 +0800

    Use gitsigns.nvim instead of git-blame.nvim for Git blame virtual text

--- private_dot_config/nvim/lua/plugins/plugins.lua
+++ private_dot_config/nvim/lua/plugins/plugins.lua
@@ -67,17 +67,23 @@ return {
     event = "BufReadPost",
   },
   {
-    "f-person/git-blame.nvim",
-    config = function()
-      vim.g.gitblame_date_format = "%r"
-      vim.g.gitblame_message_when_not_committed = ""
-    end,
-    event = "BufReadPost",
-  },
-  {
     "akinsho/git-conflict.nvim",
     config = true,
     event = "BufReadPost",
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_formatter = "<author>, <author_time> - <summary>",
+      current_line_blame_formatter_nc = "",
+      current_line_blame_formatter_opts = {
+        relative_time = true,
+      },
+      current_line_blame_opts = {
+        delay = 0,
+      },
+    },
   },
   {
     "DNLHC/glance.nvim",
