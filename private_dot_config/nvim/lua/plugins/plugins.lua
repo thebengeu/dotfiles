9b43c9beb83a8d7b2dc3f784197b594d7333eaae
Beng Eu <beng@bengeu.com>
2023-02-12 23:32:43 +0800

    Use private_dot_config/nvim/lua/plugins/plugins.luaR instead of deprecated relative_time option

--- private_dot_config/nvim/lua/plugins/plugins.lua
+++ private_dot_config/nvim/lua/plugins/plugins.lua
@@ -75,11 +75,8 @@ return {
     "lewis6991/gitsigns.nvim",
     opts = {
       current_line_blame = true,
-      current_line_blame_formatter = "<author>, <author_time> - <summary>",
+      current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
       current_line_blame_formatter_nc = "",
-      current_line_blame_formatter_opts = {
-        relative_time = true,
-      },
       current_line_blame_opts = {
         delay = 0,
       },
