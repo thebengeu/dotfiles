2e7afd42b4f40d7d81d410a2a34385a291197fa8
Beng Eu <beng@bengeu.com>
2023-02-19 03:16:40 +0800

    Improve nvim-cmp/luasnip/tabout interaction

--- private_dot_config/nvim/lua/plugins/plugins.lua
+++ private_dot_config/nvim/lua/plugins/plugins.lua
@@ -457,18 +457,14 @@ return {
           select = false,
         }),
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
+          if luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
           else
             fallback()
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
+          if luasnip.jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
@@ -967,9 +963,10 @@ return {
   },
   {
     "abecodes/tabout.nvim",
-    config = true,
-    dependencies = "hrsh7th/nvim-cmp",
     event = "InsertEnter",
+    opts = {
+      completion = false,
+    },
   },
   {
     "nvim-telescope/telescope.nvim",
