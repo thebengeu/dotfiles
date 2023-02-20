3f2f0ca0246906d4a80e834c02631fb182fba869
Beng Eu <beng@bengeu.com>
2023-02-20 22:18:23 +0800

    Set ipython ast_node_interactivity to last_expr_or_assign

--- dot_ipython/profile_default/ipython_config.py
+++ dot_ipython/profile_default/ipython_config.py
@@ -1,4 +1,5 @@
 c = get_config()
+c.InteractiveShell.ast_node_interactivity = "last_expr_or_assign"
 c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.prompt_includes_vi_mode = False
