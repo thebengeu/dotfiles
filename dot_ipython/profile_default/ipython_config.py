c0bc94ca7ca1fe32f647bcd623b00172dd88327d
Beng Eu <beng@bengeu.com>
2023-02-21 20:29:15 +0800

    Add rich to ipython extensions

--- dot_ipython/profile_default/ipython_config.py
+++ dot_ipython/profile_default/ipython_config.py
@@ -1,5 +1,6 @@
 c = get_config()
 c.InteractiveShell.ast_node_interactivity = "last_expr_or_assign"
+c.InteractiveShellApp.extensions = ["rich"]
 c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.prompt_includes_vi_mode = False
