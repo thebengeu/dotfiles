7596e69c21f5ca4ac54f3e07472109b94fa234b9
Beng Eu <beng@bengeu.com>
2023-02-25 17:49:53 +0800

    Autoreload by default in ipython

--- dot_ipython/profile_default/ipython_config.py
+++ dot_ipython/profile_default/ipython_config.py
@@ -1,6 +1,12 @@
 c = get_config()
 c.InteractiveShell.ast_node_interactivity = "last_expr_or_assign"
-c.InteractiveShellApp.extensions = ["rich"]
+c.InteractiveShellApp.exec_lines = [
+    "%autoreload 2",
+]
+c.InteractiveShellApp.extensions = [
+    "autoreload",
+    "rich",
+]
 c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.prompt_includes_vi_mode = False
