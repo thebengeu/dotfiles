0a4e1e9ac9edefaded72720b1117163b1f6130b1
Beng Eu <beng@bengeu.com>
2023-03-12 17:06:18 +0800

    Add Git push key mapping

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -10,6 +10,7 @@ vim.keymap.set(
   [[<Cmd>update | exec ":!git commit -m '" . input("") . "'" | AsyncRun -mode=term -pos=hide git push<CR>]],
   { desc = "Git commit" }
 )
+vim.keymap.set("n", "<leader>gP", [[<Cmd>AsyncRun -mode=term -pos=hide git push<CR>]], { desc = "Git push" })
 vim.keymap.set("n", "<C-r>", "<Cmd>silent redo<CR>")
 vim.keymap.set("n", "u", "<Cmd>silent undo<CR>")
 vim.keymap.set("n", "<space>bo", "<Cmd>%bd|e#|bd#<CR>", { desc = "Delete other buffers" })
