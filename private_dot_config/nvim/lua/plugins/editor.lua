bd927c02ddaea78a9a846ce678b53dc91c2a26fa
Beng Eu <beng@bengeu.com>
2023-04-01 23:35:00 +0800

    Don't delete x/X key mappings for leap.nvim

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -61,6 +61,16 @@ return {
     },
   },
   {
+    "ggandor/leap.nvim",
+    keys = {
+      { "X", mode = { "n", "x", "o" }, desc = "Leap backward before" },
+      { "x", mode = { "n", "x", "o" }, desc = "Leap forward before" },
+    },
+    config = function()
+      require("leap").add_default_mappings(true)
+    end,
+  },
+  {
     "chentoast/marks.nvim",
     event = { "BufNewFile", "BufReadPost" },
     opts = {
