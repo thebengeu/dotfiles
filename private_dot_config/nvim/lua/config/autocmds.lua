845c0af336c9b533c72c3581aad1668419fb5c23
Beng Eu <beng@bengeu.com>
2023-07-28 09:48:14 +0800

    Copy .wezterm.lua on save

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -11,6 +11,10 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   command = "!chezmoi apply --source-path <afile>",
   pattern = "*/.local/share/chezmoi/*",
 })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  command = "!cp ~/.wezterm.lua /mnt/c/Users/beng",
+  pattern = "*/.local/share/chezmoi/dot_wezterm.lua.tmpl",
+})
 vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
   command = 'call system("tmux rename-window " . expand("%:p"))',
 })
