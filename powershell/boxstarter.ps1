f37b848ddd8d98a3e7ad85de0f2fa7457676b4ff
Beng Eu <beng@bengeu.com>
2023-08-08 00:35:57 +0800

    Fix variables

--- powershell/boxstarter.ps1
+++ powershell/boxstarter.ps1
@@ -55,7 +55,7 @@ foreach ($storeApps in $storeApps)
 
 choco feature enable -n allowGlobalConfirmation
 
-@chocoPackages = @(
+$chocoPackages = @(
   'Kindle'
 )
 
@@ -67,7 +67,7 @@ foreach ($chocoPackage in $chocoPackages)
 irm get.scoop.sh | iex
 scoop bucket add extras
 
-@scoopPackages = @(
+$scoopPackages = @(
   'tableplus'
 )
 
@@ -84,7 +84,7 @@ $boxstarterPath = "${env:USERPROFILE}\boxstarter"
 
 git clone git@github.com:thebengeu/boxstarter.git $boxstarterPath
 
-@manifestPaths = @(
+$manifestPaths = @(
   'a\AudioBand\AudioBand\1.2.1'
   'r\Rabby\RabbyDesktop\0.31.0'
   't\Todoist\Todoist\8.5.0'
