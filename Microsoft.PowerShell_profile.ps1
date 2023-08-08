6a5dfdbd05d99c83e9f78e6ce0ac256949a6e159
Beng Eu <beng@bengeu.com>
2023-08-08 13:51:37 +0800

    Import zoxide and fzf

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
 Invoke-Expression (&starship init powershell)
+Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+
+Import-Module PSFzf
+
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 New-Alias which Get-Command
