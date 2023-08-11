cb68af63501dd979b7b4d4c41e26a056d775db02
Beng Eu <beng@bengeu.com>
2023-08-12 04:32:35 +0800

    Add PowerShell aliases, clean up Fish abbreviations

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,223 @@
 Invoke-Expression (&starship init powershell)
-Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+Invoke-Expression (& {
+ (zoxide init --cmd cd powershell | Out-String) }
+)
 
 Import-Module PSFzf
 Import-Module PSWindowsUpdate
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-New-Alias which Get-Command
+Set-Alias b bat
+Set-Alias cat bat
+Set-Alias g git
+Set-Alias j just
+Set-Alias lg lazygit
+Set-Alias n nvim
+Set-Alias p pnpm
+Set-Alias tg topgrade
+Set-Alias vim nvim
+Set-Alias which Get-Command
 
 $Env:NODE_NO_WARNINGS = 1
 $Env:PNPM_HOME = "$env:LOCALAPPDATA\pnpm"
 $Env:Path += @(
-    "$Env:ProgramFiles\PostgreSQL\15\bin"
-    $Env:PNPM_HOME
-    "$Env:USERPROFILE\go\bin"
+  "$Env:ProgramFiles\PostgreSQL\15\bin"
+  $Env:PNPM_HOME
+  "$Env:USERPROFILE\go\bin"
 ) -join [IO.Path]::PathSeparator
+
+function ca
+{
+  chezmoi apply --exclude templates
+}
+
+function cgca
+{
+  chezmoi git -- commit --amend
+}
+
+function cgcam
+{
+  chezmoi git -- commit -a -m $args
+}
+
+function cgd
+{
+  chezmoi git diff
+}
+
+function cgl
+{
+  chezmoi git lg
+}
+
+function cglp
+{
+  chezmoi git -- lg --patch
+}
+
+function cgP
+{
+  chezmoi git push
+}
+
+function cgp
+{
+  chezmoi git pull
+}
+
+function cgs
+{
+  chezmoi git s
+}
+
+function clg
+{
+  lazygit --path $Env:USERPROFILE/.local/share/chezmoi
+}
+
+function cn
+{
+  nvim --cmd "cd ~/.local/share/chezmoi"
+}
+
+function cr
+{
+  chezmoi re-add
+}
+
+function cu
+{
+  chezmoi update --exclude templates
+}
+
+function gaa
+{
+  git add -A
+}
+
+function gca
+{
+  git commit --amend
+}
+
+function gcam
+{
+  git commit -a -m $args
+}
+
+function gcm
+{
+  git commit -m $args
+}
+
+function gco
+{
+  git checkout $args
+}
+
+function gd
+{
+  git diff
+}
+
+function gl
+{
+  git lg
+}
+
+function glp
+{
+  git lg --patch
+}
+
+function gP
+{
+  git push
+}
+
+function gp
+{
+  git pull
+}
+
+function gr
+{
+  git rebase
+}
+
+function grbc
+{
+  git rebase --continue
+}
+
+function grhh
+{
+  git reset --hard HEAD
+}
+
+function gs
+{
+  git s
+}
+
+function gsa
+{
+  git stash apply
+}
+
+function gsP
+{
+  git stash push
+}
+
+function gsp
+{
+  git stash pop
+}
+
+function jd
+{
+  just dev
+}
+
+function pd
+{
+  pnpm dev
+}
+
+function pi
+{
+  pnpm i $args
+}
+
+function pp
+{
+  psql postgresql://postgres:postgres@localhost:5432/postgres
+}
+
+function ppg
+{
+  pnpm prisma generate
+}
+
+function prm
+{
+  pnpm rm $args
+}
+
+function rg
+{
+  & $(Get-Command -CommandType Application rg) --max-columns 1000 $args
+}
+
+function scc
+{
+  & $(Get-Command -CommandType Application scc) --not-match "package-lock.json|pnpm-lock.yaml" $args
+}
+
+function tsx
+{
+  pnpm tsx $args
+}
