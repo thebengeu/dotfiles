ac7332471252948242f8d7e5eea7b7b35f85ee4d
Beng Eu <beng@bengeu.com>
2023-08-12 02:09:31 +0800

    Add gup, topgrade and bat Windows config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,10 +2,16 @@ Invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
 
 Import-Module PSFzf
+Import-Module PSWindowsUpdate
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 New-Alias which Get-Command
 
 $Env:NODE_NO_WARNINGS = 1
-$Env:Path += [IO.Path]::PathSeparator + "$env:ProgramFiles\PostgreSQL\15\bin"
+$Env:PNPM_HOME = "$env:LOCALAPPDATA\pnpm"
+$Env:Path += @(
+    "$Env:ProgramFiles\PostgreSQL\15\bin"
+    $Env:PNPM_HOME
+    "$Env:USERPROFILE\go\bin"
+) -join [IO.Path]::PathSeparator
