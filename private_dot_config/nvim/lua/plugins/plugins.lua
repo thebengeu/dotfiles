ae4ffeba6b9317213075f48d95ac773168343d48
Beng Eu <beng@bengeu.com>
2023-08-15 17:57:42 +0800

    Use Nushell for Neovim terminal on Windows

--- private_dot_config/nvim/lua/plugins/plugins.lua
+++ private_dot_config/nvim/lua/plugins/plugins.lua
@@ -1,3 +1,13 @@
+if jit.os:find("Windows") then
+  local Util = require("lazyvim.util")
+  local float_term = Util.float_term
+
+  ---@diagnostic disable-next-line: duplicate-set-field
+  Util.float_term = function(cmd, opts)
+    return float_term(cmd or { "nu" }, opts)
+  end
+end
+
 return {
   {
     "skywind3000/asyncrun.vim",
