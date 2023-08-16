a1e17d70f23a669f842631d33ffbfde1ee9f9a77
Beng Eu <beng@bengeu.com>
2023-08-16 13:50:25 +0800

    Add cross-platform tasks to run onchange

---
+++ .chezmoitemplates/run_onchange_cross-platform.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+cargo install atuin
+cargo install broot
+cargo install cargo-update
+cargo install just
+cargo install sd
+cargo install starship
+cargo install tealdeer
+cargo install tokei
+cargo install topgrade
+cargo install xh
+cargo install zoxide
+go install github.com/nao1215/gup@latest
+pipx install howdoi
+pipx install khal
+pipx install neovim-remote
+pipx install qmk
+pipx install sqlfluff
+pnpm add --global npm-check-updates
+pnpm add --global pino-pretty
+pnpm add --global pm2
+pnpm add --global https://github.com/thebengeu/ts-node.git
+curl cht.sh/:cht.sh >"$HOME"/.local/bin/cht.sh
