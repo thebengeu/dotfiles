3b743d3ce12a9d4ac1a01d9489d44f74957bc06a
Beng Eu <beng@bengeu.com>
2023-08-17 11:18:41 +0800

    Self elevate administrator.ps1

--- powershell/administrator.ps1
+++ powershell/administrator.ps1
@@ -1,3 +1,13 @@
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator'))
+{
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000)
+  {
+    $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -Wait -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    Exit
+  }
+}
+
 function Set-Registry-Values($path, $values)
 {
   foreach ($name in $values.Keys)
