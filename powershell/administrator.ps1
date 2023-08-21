b1a63c49dc92742933c77c698c4bc6bea44b6e5d
Beng Eu <beng@bengeu.com>
2023-08-21 11:55:42 +0800

    Install zsh

--- powershell/administrator.ps1
+++ powershell/administrator.ps1
@@ -246,7 +246,7 @@ foreach ($environmentVariableTarget in $pathsForTargets.Keys)
 
 C:\msys64\usr\bin\sh --login -c 'pacman -Suy --noconfirm'
 C:\msys64\usr\bin\sh --login -c 'pacman -Suy --noconfirm'
-C:\msys64\usr\bin\sh --login -c 'pacman -S --needed --noconfirm fish tmux'
+C:\msys64\usr\bin\sh --login -c 'pacman -S --needed --noconfirm fish tmux zsh'
 
 $Env:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + [IO.Path]::PathSeparator + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
