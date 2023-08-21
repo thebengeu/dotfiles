836793fb575997bfd970e7a3d73294e19f0f88f9
Beng Eu <beng@bengeu.com>
2023-08-22 02:11:02 +0800

    Only enable tmux.nvim if in tmux

--- private_dot_config/nvim/lua/plugins/plugins.lua
+++ private_dot_config/nvim/lua/plugins/plugins.lua
@@ -91,6 +91,7 @@ return {
   {
     "aserowy/tmux.nvim",
     config = true,
+    enabled = os.getenv("TMUX") ~= nil,
     keys = {
       { "<A-j>", mode = { "i", "n", "v" } },
       { "<A-k>", mode = { "i", "n", "v" } },
