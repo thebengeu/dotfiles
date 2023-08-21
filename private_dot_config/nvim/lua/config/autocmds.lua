fdf5b5f8ce75ee04f0d042bae53e8c2b1b6acadf
Beng Eu <beng@bengeu.com>
2023-08-22 02:05:55 +0800

    Only call tmux rename-window if in tmux

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -11,9 +11,11 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   command = "AsyncRun -mode=term -pos=hide chezmoi apply --source-path <afile>",
   pattern = "*/.local/share/chezmoi/*",
 })
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
-  command = 'call system("tmux rename-window " . expand("%:p"))',
-})
+if os.getenv("TMUX") ~= nil then
+  vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
+    command = 'call system("tmux rename-window " . expand("%:p"))',
+  })
+end
 vim.api.nvim_create_autocmd("BufEnter", {
   command = "startinsert",
   pattern = "term://*",
