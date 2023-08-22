74cb37331a2026710fe8f32b01e4e6e8d3201468
Beng Eu <beng@bengeu.com>
2023-08-22 16:33:03 +0800

    Add -NoExit

--- run_onchange/run_onchange_init.ps1
+++ run_onchange/run_onchange_init.ps1
@@ -1,6 +1,6 @@
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator'))
 {
-  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`""
-  Start-Process -Wait -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`""
+  Start-Process -Wait -FilePath powershell -Verb Runas -ArgumentList $CommandLine
   Exit
 }
--- run_onchange/run_onchange_install-manifests.ps1
+++ run_onchange/run_onchange_install-manifests.ps1
@@ -1,6 +1,6 @@
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator'))
 {
-  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`""
+  $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`""
   Start-Process -Wait -FilePath pwsh -Verb Runas -ArgumentList $CommandLine
   Exit
 }
