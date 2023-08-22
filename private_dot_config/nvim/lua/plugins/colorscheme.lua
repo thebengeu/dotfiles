36f8b12c062a4c960e85786b0b4d63e6cc570334
Beng Eu <beng@bengeu.com>
2023-08-22 13:17:28 +0800

    Set float term to zsh

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -158,7 +158,7 @@ return {
 
         ---@diagnostic disable-next-line: duplicate-set-field
         Util.float_term = function(cmd, opts)
-          return float_term(cmd or { "nu" }, opts)
+          return float_term(cmd or { "C:\\msys64\\usr\\bin\\zsh", "--login" }, opts)
         end
       end
     end,
