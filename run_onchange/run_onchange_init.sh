76d8af256fb1d31cdf3933b9be1282045c8038ca
Beng Eu <beng@bengeu.com>
2023-08-22 20:57:27 +0800

    Move Ansible init to init.sh

--- run_onchange/run_onchange_init.sh
+++ run_onchange/run_onchange_init.sh
@@ -1,8 +1,35 @@
 #!/usr/bin/env sh
-go install cuelang.org/go/cmd/cue@latest
+
+if [ ! -f /usr/bin/op ]; then
+	curl -sS https://downloads.1password.com/linux/keys/1password.asc |
+		sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+		sudo tee /etc/apt/sources.list.d/1password.list
+	sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+	curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol |
+		sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+	sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+	curl -sS https://downloads.1password.com/linux/keys/1password.asc |
+		sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+	sudo apt update && sudo apt install 1password-cli
+fi
+
+export EJSON_KEYDIR="$HOME/.config/ejson/keys"
 
 EJSON_PUBLIC_KEY="5df4cad7a4c3a2937a863ecf18c56c23274cb048624bc9581ecaac56f2813107"
-EJSON_KEY_PATH="$HOME/.config/ejson/keys/$EJSON_PUBLIC_KEY"
+EJSON_KEY_PATH="$EJSON_KEYDIR/$EJSON_PUBLIC_KEY"
+SSH_KEY_PATH="$HOME/.ssh/id_ed25519"
+
+if [ ! -f ~/.ssh/id_ed25519 ] || [ ! -f "$EJSON_KEY_PATH" ]; then
+	eval "$(op signin)"
+fi
+
+if [ ! -f ~/.ssh/id_ed25519 ]; then
+	op read 'op://Personal/Ed25519 SSH Key/id_ed25519' --out-file "$SSH_KEY_PATH"
+	chmod 600 "$SSH_KEY_PATH"
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_ed25519
+fi
 
 if [ ! -f "$EJSON_KEY_PATH" ]; then
 	curl -Ls https://github.com/Shopify/ejson/releases/download/v1.4.1/ejson_1.4.1_linux_amd64.tar.gz | tar xz --directory ~/.local/bin ejson
@@ -11,4 +38,12 @@ if [ ! -f "$EJSON_KEY_PATH" ]; then
 	op read op://Personal/ejson/"$EJSON_PUBLIC_KEY" --out-file "$EJSON_KEY_PATH"
 fi
 
-EJSON_KEYDIR=~/.config/ejson/keys chezmoi apply --init
+CHEZMOI_SOURCE_DIR="$HOME/.local/share/chezmoi"
+
+git clone git@github.com:thebengeu/dotfiles.git "$CHEZMOI_SOURCE_DIR"
+
+sudo add-apt-repository ppa:ansible/ansible
+sudo apt install ansible
+ansible-playbook --tags server "$CHEZMOI_SOURCE_DIR"/site.yml --ask-become-pass
+
+chezmoi apply --init
