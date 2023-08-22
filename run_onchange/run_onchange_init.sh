d73d9e8a7693dade4bc9b603212edb44e24cad3e
Beng Eu <beng@bengeu.com>
2023-08-22 17:56:48 +0800

    Fix if condition

--- run_onchange/run_onchange_init.sh
+++ run_onchange/run_onchange_init.sh
@@ -4,7 +4,7 @@ go install cuelang.org/go/cmd/cue@latest
 EJSON_PUBLIC_KEY="5df4cad7a4c3a2937a863ecf18c56c23274cb048624bc9581ecaac56f2813107"
 EJSON_KEY_PATH="$HOME/.config/ejson/keys/$EJSON_PUBLIC_KEY"
 
-if [! -f "$EJSON_KEY_PATH" ]; then
+if [ ! -f "$EJSON_KEY_PATH" ]; then
 	curl -Ls https://github.com/Shopify/ejson/releases/download/v1.4.1/ejson_1.4.1_linux_amd64.tar.gz | tar xz --directory ~/.local/bin ejson
 	mkdir -p "$HOME"/.config/ejson/keys
 	op read op://Personal/ejson/"$EJSON_PUBLIC_KEY" --out-file "$EJSON_KEY_PATH"
