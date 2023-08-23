fdb8baaea0a3d19a52bb04406bdc912f08691721
Beng Eu <beng@bengeu.com>
2023-08-23 21:47:42 +0800

    Remove atuin

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,7 +6,7 @@ set --global fish_cursor_replace_one underscore
 set --global fish_greeting
 set --global fish_vi_force_cursor 1
 
-enable_transience
+# enable_transience
 
 abbr --add man batman
 abbr --add os 'set COMMAND $(op signin) && test -n "$COMMAND" && eval $COMMAND && set --export OP_TIME $(date +%s)'
