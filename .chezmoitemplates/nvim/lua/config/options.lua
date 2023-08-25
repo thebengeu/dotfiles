e33384a1474f912cc2b2e276aed0bb81fca3b033
Beng Eu <beng@bengeu.com>
2023-08-26 00:49:27 +0800

    Set title to file path and reorganise options

--- .chezmoitemplates/nvim/lua/config/options.lua
+++ .chezmoitemplates/nvim/lua/config/options.lua
@@ -1,10 +1,17 @@
+vim.api.nvim_create_user_command("Search", function(opts)
+  vim.fn.system("start https://www.google.com/search?q=" .. opts.fargs[1])
+end, { nargs = 1 })
+
 vim.g.db = "postgresql://postgres:postgres@localhost:5432/postgres"
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
-vim.o.report = 999
-vim.o.sessionoptions = table.concat({
+
+vim.opt.keywordprg = ":Search"
+vim.opt.linebreak = true
+vim.opt.report = 999
+vim.opt.sessionoptions = {
   "blank",
   "buffers",
   "curdir",
@@ -14,18 +21,14 @@ vim.o.sessionoptions = table.concat({
   "tabpages",
   "winpos",
   "winsize",
-}, ",")
-vim.opt.linebreak = true
-vim.opt.wrap = true
+}
 
 if jit.os:find("Windows") then
-  vim.o.shell = "sh"
-  vim.o.shellcmdflag = "-c"
-  vim.o.shellxquote = ""
+  vim.opt.shell = "sh"
+  vim.opt.shellcmdflag = "-c"
+  vim.opt.shellxquote = ""
 end
 
-vim.api.nvim_create_user_command("Search", function(opts)
-  vim.fn.system("start https://www.google.com/search?q=" .. opts.fargs[1])
-end, { nargs = 1 })
-
-vim.o.keywordprg = ":Search"
+vim.opt.title = true
+vim.opt.titlestring = "%F"
+vim.opt.wrap = true
