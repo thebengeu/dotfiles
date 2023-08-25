011049600bc3990e21ad4b7c08fc3682c5579983
Beng Eu <beng@bengeu.com>
2023-08-25 18:58:30 +0800

    Support OSC 133 in Fish, add Wezterm mappings

--- private_dot_config/private_fish/conf.d/shell-integration.fish
+++ private_dot_config/private_fish/conf.d/shell-integration.fish
@@ -10,3 +10,21 @@ function __on_fish_prompt --on-event fish_prompt
 
     __update_cwd_osc
 end
+
+function __mark_prompt_start --on-event fish_prompt --on-event fish_cancel --on-event fish_posterror
+    test "$__prompt_state" != prompt-start
+    and echo -en "\e]133;D\a"
+    set --global __prompt_state prompt-start
+    echo -en "\e]133;A\a"
+end
+__mark_prompt_start
+
+function __mark_output_start --on-event fish_preexec
+    set --global __prompt_state pre-exec
+    echo -en "\e]133;C\a"
+end
+
+function __mark_output_end --on-event fish_postexec
+    set --global __prompt_state post-exec
+    echo -en "\e]133;D;$status\a"
+end
