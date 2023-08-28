2bd676297d6e175d188d3edf4d2286ae93104334
Beng Eu <beng@bengeu.com>
2023-08-28 13:06:24 +0800

    Use deprecated vim.lsp.get_active_clients() for backward compatibility

--- .chezmoitemplates/nvim/lua/plugins/lsp.lua
+++ .chezmoitemplates/nvim/lua/plugins/lsp.lua
@@ -116,7 +116,7 @@ return {
                 return
               end
 
-              local client = vim.lsp.get_clients({
+              local client = vim.lsp.get_active_clients({
                 bufnr = event.buf,
                 name = "eslint",
               })[1]
