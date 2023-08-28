2d372749605d14bf7d4277d17c31f3ce4067a4be
Beng Eu <beng@bengeu.com>
2023-08-29 00:05:26 +0800

    Refactor keymaps.lua

--- .chezmoitemplates/nvim/lua/config/keymaps.lua
+++ .chezmoitemplates/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
+local edit_chezmoi_path = function(source_or_target)
+  return function()
+    vim.cmd("edit " .. vim.fn.system({ "chezmoi", source_or_target .. "-path", vim.api.nvim_buf_get_name(0) }))
+  end
+end
+
 local update_commit_push = function(flags)
   return function()
     vim.api.nvim_command("update")
@@ -11,6 +17,9 @@ local update_commit_push = function(flags)
   end
 end
 
+vim.keymap.del("x", "j")
+vim.keymap.del("x", "k")
+
 vim.keymap.set("n", "<leader>ga", update_commit_push("a"), { desc = "Git commit all" })
 vim.keymap.set("n", "<leader>gc", update_commit_push(""), { desc = "Git commit" })
 vim.keymap.set("n", "<leader>gP", [[<Cmd>AsyncRun -close -mode=term -rows=5 git push<CR>]], { desc = "Git push" })
@@ -18,11 +27,5 @@ vim.keymap.set("n", "<leader>gp", [[<Cmd>AsyncRun -close -mode=term -rows=5 git 
 vim.keymap.set("n", "<C-r>", "<Cmd>silent redo<CR>")
 vim.keymap.set("n", "u", "<Cmd>silent undo<CR>")
 vim.keymap.set("n", "<space>bo", "<Cmd>%bd|e#|bd#<CR>", { desc = "Delete other buffers" })
-vim.keymap.del("x", "j")
-vim.keymap.del("x", "k")
-vim.keymap.set("n", "<space>cs", function()
-  vim.cmd("edit " .. vim.fn.system({ "chezmoi", "source-path", vim.api.nvim_buf_get_name(0) }))
-end, { desc = "Chezmoi Source" })
-vim.keymap.set("n", "<space>ct", function()
-  vim.cmd("edit " .. vim.fn.system({ "chezmoi", "target-path", vim.api.nvim_buf_get_name(0) }))
-end, { desc = "Chezmoi Target" })
+vim.keymap.set("n", "<space>cs", edit_chezmoi_path("source"), { desc = "Chezmoi Source" })
+vim.keymap.set("n", "<space>ct", edit_chezmoi_path("target"), { desc = "Chezmoi Target" })
