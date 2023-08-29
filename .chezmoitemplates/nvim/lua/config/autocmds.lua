3896d0a22a76a4465ee056f13d17f53cda29ff2a
Beng Eu <beng@bengeu.com>
2023-08-30 02:03:50 +0800

    Use callback instead of command for tmux autocmd

--- .chezmoitemplates/nvim/lua/config/autocmds.lua
+++ .chezmoitemplates/nvim/lua/config/autocmds.lua
@@ -1,5 +1,15 @@
 local async_run = require("util").async_run
 
+vim.api.nvim_create_autocmd("BufEnter", {
+  command = "startinsert",
+  pattern = "term://*",
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  callback = async_run("chezmoi apply --init"),
+  pattern = "*/.local/share/chezmoi/*",
+})
+
 vim.api.nvim_create_autocmd("TextChanged", {
   callback = function()
     if vim.fn.getline("."):find("^%s*,%s*$") then
@@ -9,16 +19,11 @@ vim.api.nvim_create_autocmd("TextChanged", {
   end,
   pattern = "*.lua",
 })
-vim.api.nvim_create_autocmd("BufWritePost", {
-  callback = async_run("chezmoi apply --init"),
-  pattern = "*/.local/share/chezmoi/*",
-})
+
 if os.getenv("TMUX") ~= nil then
   vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
-    command = 'call system("tmux rename-window " . expand("%:p"))',
+    callback = function()
+      vim.fn.system("tmux rename-window " .. vim.fn.expand("%:p"))
+    end,
   })
 end
-vim.api.nvim_create_autocmd("BufEnter", {
-  command = "startinsert",
-  pattern = "term://*",
-})
