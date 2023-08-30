fc9949eb2397edc089e58643fd237caec252b2e2
Beng Eu <beng@bengeu.com>
2023-08-30 13:50:33 +0800

    Add nvim-spider

--- .chezmoitemplates/nvim/lua/plugins/editor.lua
+++ .chezmoitemplates/nvim/lua/plugins/editor.lua
@@ -71,6 +71,15 @@ return {
     },
   },
   {
+    "chrisgrieser/nvim-spider",
+    keys = {
+      { "w", "<cmd>lua require('spider').motion('w')<CR>" },
+      { "e", "<cmd>lua require('spider').motion('e')<CR>" },
+      { "b", "<cmd>lua require('spider').motion('b')<CR>" },
+      { "ge", "<cmd>lua require('spider').motion('ge')<CR>" },
+    },
+  },
+  {
     "linty-org/readline.nvim",
     keys = {
       {
