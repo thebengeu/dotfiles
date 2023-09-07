c24b2b85a3114e73ebde0569e358135e6479f409
Beng Eu <beng@bengeu.com>
2023-09-07 11:34:32 +0800

    Switch Telescope sorting strategy back to descending

--- .chezmoitemplates/nvim/lua/plugins/telescope.lua
+++ .chezmoitemplates/nvim/lua/plugins/telescope.lua
@@ -112,7 +112,6 @@ return {
           },
         },
         layout_strategy = "flex",
-        sorting_strategy = "ascending",
         vimgrep_arguments = vim.list_extend(
           vim.fn.copy(require("telescope.config").values.vimgrep_arguments),
           { "--hidden" }
