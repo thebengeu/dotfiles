88d80e84ebedb439e1768e2d2ec58402a199fbae
Beng Eu <beng@bengeu.com>
2023-09-09 04:12:36 +0800

    Move to .generated.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,7 @@ Import-Module PSWindowsUpdate
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-. $Env:USERPROFILE\generated.ps1
+. $Env:USERPROFILE\.generated.ps1
 
 Set-Alias touch New-Item
 Set-Alias which Get-Command
