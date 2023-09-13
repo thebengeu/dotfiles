b3a9c6c417dd8f37bf00a01f7303f2b48794a643
Beng Eu <beng@bengeu.com>
2023-09-13 11:07:09 +0800

    Setup AutoHotkey hotkeys to activate WezTerm windows and tabs

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -102,6 +102,30 @@ config.keys = {
 	{ key = "l", mods = "SHIFT|ALT|CTRL", action = act.ActivatePaneDirection("Right") },
 	{ key = "k", mods = "SHIFT|ALT|CTRL", action = act.ActivatePaneDirection("Up") },
 	{ key = "j", mods = "SHIFT|ALT|CTRL", action = act.ActivatePaneDirection("Down") },
+	{ key = "!", mods = "SHIFT|ALT", action = act.Nop },
+	{ key = "@", mods = "SHIFT|ALT", action = act.Nop },
+	{ key = "#", mods = "SHIFT|ALT", action = act.Nop },
+	{ key = "$", mods = "SHIFT|ALT", action = act.Nop },
+	{ key = "%", mods = "SHIFT|ALT", action = act.Nop },
+	{ key = "^", mods = "SHIFT|ALT", action = act.Nop },
+	{ key = "&", mods = "SHIFT|ALT", action = act.Nop },
+	{ key = "*", mods = "SHIFT|ALT", action = act.Nop },
+	{ key = "(", mods = "SHIFT|ALT", action = act.Nop },
+	{ key = ")", mods = "SHIFT|ALT", action = act.Nop },
+	{ key = "!", mods = "SHIFT|ALT|CTRL", action = act.ActivateWindow(0) },
+	{
+		key = "@",
+		mods = "SHIFT|ALT|CTRL",
+		action = wezterm.action_callback(function()
+			local gui_window = wezterm.gui.gui_windows()[2]
+
+			if gui_window then
+				gui_window:focus()
+			else
+				wezterm.mux.spawn_window({})
+			end
+		end),
+	},
 	split_nav("move", "h"),
 	split_nav("move", "j"),
 	split_nav("move", "k"),
