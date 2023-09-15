ed273faeb261225d17832f8f63bbe812fcc36a8d
Beng Eu <beng@bengeu.com>
2023-09-16 00:19:43 +0800

    Rename to activate-wezterm-tab.ahk

--- dot_local/bin/executable_nr.sh
+++ dot_local/bin/executable_nr.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-"$PROGRAMFILES\AutoHotkey\v2\AutoHotkey64.exe" "$APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\AutoHotkey.ahk" n 0
+"$PROGRAMFILES\AutoHotkey\v2\AutoHotkey64.exe" "$APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\activate-wezterm-tab.ahk" n 0
 sh ~/.local/bin/nvr-latest-focused-nvim.sh "$@"
