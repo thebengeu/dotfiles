5fbce04c7cbeddb9e0748bb3500483592d09d56b
Beng Eu <beng@bengeu.com>
2023-09-17 10:04:30 +0800

    Set pinned shortcut arguments automatically

--- run_after-tweak-shortcuts.ps1
+++ run_after-tweak-shortcuts.ps1
@@ -3,17 +3,33 @@ Remove-Item $env:PUBLIC\Desktop\*.lnk
 
 $startMenuPrograms = 'Microsoft\Windows\Start Menu\Programs'
 
-$shortcutArguments = @{
+$startMenuShortcutArguments = @{
   "$Env:APPDATA\$startMenuPrograms\Chromium.lnk"    = '--proxy-server=zproxy.lum-superproxy.io:22225'
 }
 
-foreach ($shortcutPath in $shortcutArguments.Keys)
+foreach ($shortcutPath in $startMenuShortcutArguments.Keys)
 {
   $shortcut = (New-Object -ComObject WScript.Shell).CreateShortCut($shortcutPath)
-  $shortcut.Arguments = $shortcutArguments[$shortcutPath]
+  $shortcut.Arguments = $startMenuShortcutArguments[$shortcutPath]
   $shortcut.Save()
 }
 
 $shortcut = (New-Object -ComObject WScript.Shell).CreateShortCut("$Env:APPDATA\$startMenuPrograms\Startup\dual-key-remap.lnk")
 $shortcut.TargetPath = "$Env:USERPROFILE\.local\bin\dual-key-remap.exe"
 $shortcut.Save()
+
+$pinnedShortcutArguments = @{
+  "Chromium" = '--proxy-server=zproxy.lum-superproxy.io:22225'
+}
+
+foreach ($appName in $pinnedShortcutArguments.Keys)
+{
+  $shortcutPath = "$Env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\$appName.lnk"
+
+  if ((Test-Path $shortcutPath))
+  {
+    $shortcut = (New-Object -ComObject WScript.Shell).CreateShortCut($shortcutPath)
+    $shortcut.Arguments = $pinnedShortcutArguments[$appName]
+    $shortcut.Save()
+  }
+}
