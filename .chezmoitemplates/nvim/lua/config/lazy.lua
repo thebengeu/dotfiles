6f6896650d30dfe3268eae5be2d0d87a99825a96
Beng Eu <beng@bengeu.com>
2023-09-18 17:00:11 +0800

    Import lazyvim mini-files extra

--- .chezmoitemplates/nvim/lua/config/lazy.lua
+++ .chezmoitemplates/nvim/lua/config/lazy.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.dap" },
+    { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
