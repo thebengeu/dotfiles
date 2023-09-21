16dfb9cfea9205aa9e48e6e86a6434c6a0087914
Beng Eu <beng@bengeu.com>
2023-09-22 00:15:01 +0800

    Add icons to WezTerm tab titles

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -430,4 +430,44 @@ config.window_padding = {
   bottom = 0,
 }
 
+local nerdfonts = wezterm.nerdfonts
+local process_name_icons = {
+  [""] = nerdfonts.cod_debug_console,
+  bash = nerdfonts.cod_terminal_bash,
+  fish = nerdfonts.md_fish,
+  pwsh = nerdfonts.cod_terminal_powershell,
+  powershell = nerdfonts.cod_terminal_powershell,
+  zsh = nerdfonts.cod_terminal,
+}
+
+wezterm.on("format-tab-title", function(tab)
+  local active_pane = tab.active_pane
+  local foreground_process_name = active_pane.foreground_process_name
+    :gsub("%.exe$", "")
+    :gsub(".-(%w+)$", "%1")
+  local user_vars = active_pane.user_vars
+  local hostname = user_vars.WEZTERM_HOSTNAME
+  local prog = (user_vars.WEZTERM_PROG or ""):gsub(" .*", "")
+
+  local icon = prog == "nvim" and nerdfonts.custom_vim
+    or (
+      hostname == (wezterm.hostname() .. "-wsl")
+        and nerdfonts.cod_terminal_linux
+      or (
+        user_vars.WEZTERM_IN_TMUX == "1" and nerdfonts.cod_terminal_tmux
+        or (
+          (hostname and hostname ~= wezterm.hostname() and nerdfonts.md_ssh)
+          or process_name_icons[foreground_process_name]
+          or ""
+        )
+      )
+    )
+
+  return tab.tab_index + 1
+    .. ": "
+    .. icon
+    .. " "
+    .. active_pane.title:gsub("%.exe$", "")
+end)
+
 return config
