772629222fc5c63c313e2e2bee31a43cb5c81c54
Beng Eu <beng@bengeu.com>
2023-09-26 17:32:55 +0800

    Add visual selection keymaps to stage/reset/undo stage hunks

--- .chezmoitemplates/nvim/lua/plugins/git.lua
+++ .chezmoitemplates/nvim/lua/plugins/git.lua
@@ -33,17 +33,23 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = {
-      on_attach = function(buffer)
+      on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
 
-        local function map(mode, l, r, desc)
-          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+        local map = function(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+        end
+
+        local gs_visual = function(operation)
+          return function()
+            gs[operation]({ vim.fn.line("."), vim.fn.line("v") })
+          end
         end
 
         map("n", "]h", gs.next_hunk, "Next Hunk")
         map("n", "[h", gs.prev_hunk, "Prev Hunk")
-        map({ "n", "v" }, "<leader>hs", gs.stage_hunk, "Stage Hunk")
-        map({ "n", "v" }, "<leader>hr", gs.reset_hunk, "Reset Hunk")
+        map("n", "<leader>hs", gs.stage_hunk, "Stage Hunk")
+        map("n", "<leader>hr", gs.reset_hunk, "Reset Hunk")
         map("n", "<leader>hS", gs.stage_buffer, "Stage Buffer")
         map("n", "<leader>hu", gs.undo_stage_hunk, "Undo Stage Hunk")
         map("n", "<leader>hR", gs.reset_buffer, "Reset Buffer")
@@ -61,6 +67,9 @@ return {
           ":<C-U>Gitsigns select_hunk<CR>",
           "GitSigns Select Hunk"
         )
+        map("x", "<leader>hs", gs_visual("stage_hunk"), "Stage Hunk")
+        map("x", "<leader>hr", gs_visual("reset_hunk"), "Reset Hunk")
+        map("x", "<leader>hu", gs_visual("undo_stage_hunk"), "Undo Stage Hunk")
       end,
     },
   },
