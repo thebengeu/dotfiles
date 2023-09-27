b469bdf53bc7f0b72986e4c9b3f23b919c895be9
Beng Eu <beng@bengeu.com>
2023-09-28 02:06:08 +0800

    Hide end of buffer tildes

--- .chezmoitemplates/nvim/lua/config/options.lua
+++ .chezmoitemplates/nvim/lua/config/options.lua
@@ -13,6 +13,7 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
 vim.opt.concealcursor = "cinv"
+vim.opt.fillchars = { eob = " " }
 vim.opt.gdefault = true
 vim.opt.guifont = "JetBrainsMono Nerd Font:h12"
 vim.opt.keywordprg = ":Search"
