d849a08df2f85b3831a7d62be56f0a7150c5e693
Beng Eu <beng@bengeu.com>
2023-09-27 23:58:27 +0800

    Load rainbow-delimiters.nvim on VeryLazy event, wrap callback in vim.schedule
    This guarantees that the config function is called after the colorscheme's, and that the autocmd's callback is called after the colorscheme's

--- .chezmoitemplates/nvim/lua/plugins/ui.lua
+++ .chezmoitemplates/nvim/lua/plugins/ui.lua
@@ -142,29 +142,31 @@ return {
           )
         end
 
-        if hl_is_not_default(rainbow_delimiters_hl[1]) then
-          return
-        end
+        vim.schedule(function()
+          if hl_is_not_default(rainbow_delimiters_hl[1]) then
+            return
+          end
 
-        local legacy_rainbow_hl = (
-          hl_is_not_default(ts_rainbow_2_hl[1]) and ts_rainbow_2_hl
-        )
-          or (hl_is_not_default(ts_rainbow_hl[1]) and ts_rainbow_hl)
-
-        if not legacy_rainbow_hl then
-          error("No rainbow highlight groups found")
-        end
-
-        for i = 1, 7 do
-          vim.api.nvim_set_hl(
-            0,
-            rainbow_delimiters_hl[i],
-            { link = legacy_rainbow_hl[i] }
+          local legacy_rainbow_hl = (
+            hl_is_not_default(ts_rainbow_2_hl[1]) and ts_rainbow_2_hl
           )
-        end
+            or (hl_is_not_default(ts_rainbow_hl[1]) and ts_rainbow_hl)
+
+          if not legacy_rainbow_hl then
+            error("No rainbow highlight groups found")
+          end
+
+          for i = 1, 7 do
+            vim.api.nvim_set_hl(
+              0,
+              rainbow_delimiters_hl[i],
+              { link = legacy_rainbow_hl[i] }
+            )
+          end
+        end)
       end)()
     end,
-    event = { "BufNewFile", "BufReadPost" },
+    event = "VeryLazy",
   },
   {
     "lewis6991/satellite.nvim",
