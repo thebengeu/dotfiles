3d46c134aca5ef37b088285ce8db3b33a25105e6
Beng Eu <beng@bengeu.com>
2023-10-07 23:18:21 +0800

    Extract keys_pane module

--- dot_config/wezterm/common.lua
+++ dot_config/wezterm/common.lua
@@ -1,4 +1,27 @@
+local wezterm = require("wezterm")
+
 local M = {}
+
+M.activate_pane = function(pane)
+  if pane then
+    pane:window():gui_window():focus()
+    pane:activate()
+  end
+end
+
+M.find_pane = function(callback)
+  for _, window in ipairs(wezterm.gui.gui_windows()) do
+    for _, tab in ipairs(window:mux_window():tabs()) do
+      for _, pane in ipairs(tab:panes()) do
+        local found_pane = callback(pane)
+
+        if found_pane then
+          return pane
+        end
+      end
+    end
+  end
+end
 
 M.tmux_detached_session =
   "tmux has-session -t 0 2>/dev/null || tmux new-session -c ~ -d -s 0"
--- dot_config/wezterm/keys.lua
+++ dot_config/wezterm/keys.lua
@@ -1,131 +1,8 @@
+local common = require("common")
+local keys_pane = require("keys_pane")
 local keys_split_nav = require("keys_split_nav")
 local wezterm = require("wezterm")
 local act = wezterm.action
-
-local find_pane = function(callback)
-  for _, window in ipairs(wezterm.gui.gui_windows()) do
-    for _, tab in ipairs(window:mux_window():tabs()) do
-      for _, pane in ipairs(tab:panes()) do
-        local found_pane = callback(pane)
-
-        if found_pane then
-          return pane
-        end
-      end
-    end
-  end
-end
-
-local activate_pane = function(pane)
-  if pane then
-    pane:window():gui_window():focus()
-    pane:activate()
-  end
-end
-
-local activate_or_spawn_pane = function(hostname, domain_name)
-  domain_name = domain_name or ("SSHMUX:" .. hostname)
-
-  return wezterm.action_callback(function(window, event_pane)
-    local latest_prompt_time = 0
-    local latest_prompt_pane
-
-    find_pane(function(pane)
-      local matched
-      local user_vars = pane:get_user_vars()
-
-      if user_vars.WEZTERM_HOSTNAME then
-        matched = user_vars.WEZTERM_HOSTNAME == hostname
-          and user_vars.WEZTERM_PROG == ""
-      else
-        matched = (pane:get_domain_name() == domain_name)
-      end
-
-      if matched then
-        local prompt_time = tonumber(user_vars.PROMPT_TIME) or 0
-
-        if prompt_time >= latest_prompt_time then
-          latest_prompt_time = prompt_time
-          latest_prompt_pane = pane
-        end
-      end
-    end)
-
-    activate_pane(latest_prompt_pane)
-
-    if not latest_prompt_pane then
-      if domain_name:find("^SSHMUX%:") then
-        local tab_size = window:active_tab():get_size()
-
-        window:perform_action(act.AttachDomain(domain_name), event_pane)
-
-        local domain_pane
-
-        while not domain_pane do
-          wezterm.sleep_ms(1)
-          domain_pane = find_pane(function(pane)
-            return pane:get_domain_name() == domain_name
-          end)
-        end
-
-        activate_pane(domain_pane)
-
-        local domain_tab_size = domain_pane:tab():get_size()
-
-        if
-          domain_tab_size.pixel_height ~= tab_size.pixel_height
-          or domain_tab_size.pixel_width ~= tab_size.pixel_width
-        then
-          local window_dimensions = window:get_dimensions()
-
-          if
-            wezterm.hostname() == "yoga"
-            and window_dimensions.pixel_width == 3840
-            and window_dimensions.pixel_height == 2320
-          then
-            wezterm.sleep_ms(100)
-            window:toggle_fullscreen()
-            window:toggle_fullscreen()
-          else
-            wezterm.sleep_ms(10)
-            window:set_inner_size(
-              window_dimensions.pixel_width - 1,
-              window_dimensions.pixel_height - 59
-            )
-            window:set_inner_size(
-              window_dimensions.pixel_width,
-              window_dimensions.pixel_height - 58
-            )
-          end
-        end
-      else
-        window:mux_window():spawn_tab({
-          domain = {
-            DomainName = domain_name,
-          },
-        })
-      end
-    end
-  end)
-end
-
-local split_pane = function(domain_name, direction)
-  return wezterm.action_callback(function(window, pane)
-    local sshmux_hostname = pane:get_domain_name():match("^SSHMUX:(.*)")
-
-    if sshmux_hostname then
-      local _, spawned_pane, _ = window:mux_window():spawn_tab({
-        domain = { DomainName = "SSH:" .. sshmux_hostname },
-      })
-      pane = spawned_pane
-    end
-
-    pane:split({
-      direction = direction or "Right",
-      domain = { DomainName = domain_name },
-    })
-  end)
-end
 
 local M = {}
 
@@ -170,16 +47,6 @@ function M.apply_to_config(config)
       action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
     },
     {
-      key = "|",
-      mods = "SHIFT|ALT|CTRL",
-      action = split_pane("SSH:wsl"),
-    },
-    {
-      key = "_",
-      mods = "SHIFT|ALT|CTRL",
-      action = split_pane("SSH:wsl", "Bottom"),
-    },
-    {
       key = "h",
       mods = "SHIFT|ALT",
       action = act.ActivatePaneDirection("Left"),
@@ -199,69 +66,6 @@ function M.apply_to_config(config)
       mods = "SHIFT|ALT",
       action = act.ActivatePaneDirection("Down"),
     },
-    {
-      key = "d",
-      mods = "SHIFT|ALT",
-      action = activate_or_spawn_pane("dev"),
-    },
-    {
-      key = "d",
-      mods = "SHIFT|ALT|CTRL",
-      action = split_pane("SSH:dev"),
-    },
-    {
-      key = "e",
-      mods = "SHIFT|ALT",
-      action = activate_or_spawn_pane("ec2"),
-    },
-    {
-      key = "e",
-      mods = "SHIFT|ALT|CTRL",
-      action = split_pane("SSH:ec2"),
-    },
-    {
-      key = "a",
-      mods = "SHIFT|ALT",
-      action = activate_or_spawn_pane(wezterm.hostname(), "local"),
-    },
-    {
-      key = "a",
-      mods = "SHIFT|ALT|CTRL",
-      action = split_pane("local"),
-    },
-    {
-      key = "p",
-      mods = "SHIFT|ALT",
-      action = activate_or_spawn_pane("prod"),
-    },
-    {
-      key = "p",
-      mods = "SHIFT|ALT|CTRL",
-      action = split_pane("SSH:prod"),
-    },
-    {
-      key = "v",
-      mods = "SHIFT|ALT",
-      action = activate_or_spawn_pane("dev-wsl"),
-    },
-    {
-      key = "v",
-      mods = "SHIFT|ALT|CTRL",
-      action = split_pane("SSH:dev-wsl"),
-    },
-    {
-      key = "w",
-      mods = "SHIFT|ALT",
-      action = activate_or_spawn_pane(
-        wezterm.hostname() .. "-wsl",
-        "SSHMUX:wsl"
-      ),
-    },
-    {
-      key = "w",
-      mods = "SHIFT|ALT|CTRL",
-      action = split_pane("SSH:wsl"),
-    },
   }
 
   local add_nvim_key = function(mods, hostname, home_dir)
@@ -273,7 +77,7 @@ function M.apply_to_config(config)
         local latest_focused_nvim_pane
         local latest_focused_nvim_port
 
-        find_pane(function(pane)
+        common.find_pane(function(pane)
           local user_vars = pane:get_user_vars()
 
           if user_vars.WEZTERM_HOSTNAME == hostname then
@@ -290,7 +94,7 @@ function M.apply_to_config(config)
           end
         end)
 
-        activate_pane(latest_focused_nvim_pane)
+        common.activate_pane(latest_focused_nvim_pane)
 
         local file =
           io.open(home_dir .. "/.local/bin/nvr-latest-focused-nvim.sh", "w")
@@ -392,6 +196,7 @@ function M.apply_to_config(config)
     })
   end
 
+  keys_pane.apply_to_config(config)
   keys_split_nav.apply_to_config(config)
 end
 
