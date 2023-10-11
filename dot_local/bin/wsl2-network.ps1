329d811452737cf6eff685edd92ac82c0a14069f
Beng Eu <beng@bengeu.com>
2023-10-11 11:57:43 +0800

    Add script to setup firewall and port forwarding for WSL ports

---
+++ dot_local/bin/wsl2-network.ps1
@@ -0,0 +1,27 @@
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  if (Get-Command gsudo)
+  {
+    gsudo "& '$($MyInvocation.MyCommand.Path)'"
+  } else
+  {
+    $CommandLine = "-NoExit -NoProfile -File `"" + $MyInvocation.MyCommand.Path + "`""
+    Start-Process -Wait -FilePath powershell -Verb Runas -ArgumentList $CommandLine
+  }
+  Exit
+}
+
+$connectAddress = $(wsl sh -c "hostname -I | cut --delimiter ' ' --fields 1")
+$displayName = 'WSL 2 Firewall Unlock'
+$ports = 24,3000
+
+Remove-NetFireWallRule -DisplayName $displayName
+New-NetFireWallRule -Action Allow -Direction Inbound -DisplayName $displayName -LocalPort $ports -Protocol TCP
+
+foreach ($port in $ports)
+{
+  netsh interface portproxy delete v4tov4 listenaddress=0.0.0.0 listenport=$port
+  netsh interface portproxy add v4tov4 connectaddress=$connectAddress connectport=$port listenaddress=0.0.0.0 listenport=$port
+}
+
+netsh interface portproxy show v4tov4
