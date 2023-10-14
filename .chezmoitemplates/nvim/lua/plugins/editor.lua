dad55f4b2f7fc8788a77fa111a8c4843e026dfa2
Beng Eu <beng@bengeu.com>
2023-10-14 16:45:53 +0800

    Switch to ultimate-autopair.nvim

--- .chezmoitemplates/nvim/lua/plugins/editor.lua
+++ .chezmoitemplates/nvim/lua/plugins/editor.lua
@@ -140,14 +140,6 @@ return {
     enabled = false,
   },
   {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {
-      map_c_h = true,
-      map_c_w = true,
-    },
-  },
-  {
     "kevinhwang91/nvim-fundo",
     dependencies = "kevinhwang91/promise-async",
     event = "LazyFile",
@@ -334,6 +326,11 @@ return {
   {
     "cappyzawa/trim.nvim",
     event = "BufWritePre",
+    opts = {},
+  },
+  {
+    "altermo/ultimate-autopair.nvim",
+    event = { "CmdlineEnter", "InsertEnter" },
     opts = {},
   },
   {
