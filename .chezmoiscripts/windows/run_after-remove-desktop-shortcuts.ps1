5f5c450ae6066ae7f0b2e8c5632987c42a41251a
Beng Eu <beng@bengeu.com>
2023-10-18 15:57:32 +0800

    Shift around shortcut creation to run in correct order

--- .chezmoiscripts/windows/run_after-remove-desktop-shortcuts.ps1
+++ .chezmoiscripts/windows/run_after-remove-desktop-shortcuts.ps1
@@ -1,2 +1,8 @@
 Remove-Item $Env:OneDrive\Desktop\*.lnk
 Remove-Item $Env:PUBLIC\Desktop\*.lnk
+
+$startMenuPrograms = 'Microsoft\Windows\Start Menu\Programs'
+
+$shortcut = (New-Object -ComObject WScript.Shell).CreateShortCut("$Env:APPDATA\$startMenuPrograms\Startup\dual-key-remap.lnk")
+$shortcut.TargetPath = "$Env:USERPROFILE\.local\bin\dual-key-remap.exe"
+$shortcut.Save()
