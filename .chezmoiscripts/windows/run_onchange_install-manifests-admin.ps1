067ec232049a0690b2b43b70404cc9a1d26df6e1
Beng Eu <beng@bengeu.com>
2023-10-31 23:22:05 +0800

    Update manifests

--- .chezmoiscripts/windows/run_onchange_install-manifests-admin.ps1
+++ .chezmoiscripts/windows/run_onchange_install-manifests-admin.ps1
@@ -15,24 +15,24 @@ $isMobile = (Get-CimInstance -Class Win32_ComputerSystem -Property PCSystemType)
 
 $manifestPaths = @{
   'a\AudioBand\AudioBand' = @{
-    id = '0BD4E347-1EE9-4F74-BB18-067B8736E44D'
-    version = '1.2.1'
+    id = '466BAF12-E5C7-4FAC-A837-5195A0697AC6'
+    version = '1.2.3'
   }
   'r\Rabby\RabbyDesktop' = @{
     id = '2cd4acdc-36c3-5e85-b6bd-84403600b4d8'
-    version = '0.31.0'
+    version = '0.35.3'
   }
   't\Todoist\Todoist' = @{
     id = 'Doist.Todoist'
-    version = '8.5.0'
+    version = '8.9.3'
   }
 }
 
 if (!$isMobile)
 {
   $manifestPaths['f\Finkitd\o\o\ManicTimeServer']= @{
-    id = '3D16BC61-F4C6-441D-A345-3A1E0F250A6D'
-    version = '23.2.4.1'
+    id = '110013C5-52EF-40E2-9A0E-279B85062D15'
+    version = '23.3.1.1'
   }
 }
 
