1c7ed715a2e7d60a065750326a91017509492220
Beng Eu <beng@bengeu.com>
2023-11-08 23:54:30 +0800

    Install dbmate on Linux and Darwin

--- .chezmoiscripts/linux/run_onchange_nix.sh
+++ .chezmoiscripts/linux/run_onchange_nix.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nix profile install nixpkgs#{bandwhich,direnv,fennel,hledger,hledger-ui,hledger-web,ledger,less,lf,miller,nethogs,nix,nnn,nushell,peco,vifm}
+nix profile install nixpkgs#{bandwhich,dbmate,direnv,fennel,hledger,hledger-ui,hledger-web,ledger,less,lf,miller,nethogs,nix,nnn,nushell,peco,vifm}
