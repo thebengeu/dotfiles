870894b1dc07275b74fdd6c7a45391afaa18d645
Beng Eu <beng@bengeu.com>
2023-12-26 13:58:52 +0800

    fix: run `nix profile upgrade '.*'` before install

--- .chezmoiscripts/linux/run_onchange_nix.sh
+++ .chezmoiscripts/linux/run_onchange_nix.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+nix profile upgrade '.*'
 nix profile install nixpkgs#{bandwhich,dbmate,delta,fennel,fzf,hledger,hledger-ui,hledger-web,k6,ledger,less,lf,miller,nethogs,nix,nnn,nushell,peco,pspg,tmux,vifm}
