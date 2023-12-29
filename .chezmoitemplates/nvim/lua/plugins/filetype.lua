f52e6c8350c1d08ea249fd4b05535fdb4403ae5b
Beng Eu <beng@bengeu.com>
2023-12-30 01:30:49 +0800

    feat: add kmonad-vim

--- .chezmoitemplates/nvim/lua/plugins/filetype.lua
+++ .chezmoitemplates/nvim/lua/plugins/filetype.lua
@@ -8,6 +8,10 @@ return {
     cond = vim.loop.cwd():match("chezmoi") ~= nil,
   },
   {
+    "kmonad/kmonad-vim",
+    ft = "kbd",
+  },
+  {
     "LhKipp/nvim-nu",
     build = ":TSInstall nu",
     ft = "nu",
