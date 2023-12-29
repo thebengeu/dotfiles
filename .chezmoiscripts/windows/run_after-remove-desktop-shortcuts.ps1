1058de045739b8744d6326db1593c451ddd10883
Beng Eu <beng@bengeu.com>
2023-12-30 02:02:58 +0800

    chore: remove dual-key-remap

--- .chezmoiscripts/windows/run_after-remove-desktop-shortcuts.ps1
+++ .chezmoiscripts/windows/run_after-remove-desktop-shortcuts.ps1
@@ -1,8 +1,2 @@
 Remove-Item $Env:OneDrive\Desktop\*.lnk
 Remove-Item $Env:PUBLIC\Desktop\*.lnk
-
-$startMenuPrograms = 'Microsoft\Windows\Start Menu\Programs'
-
-$shortcut = (New-Object -ComObject WScript.Shell).CreateShortCut("$Env:APPDATA\$startMenuPrograms\Startup\dual-key-remap.lnk")
-$shortcut.TargetPath = "$Env:USERPROFILE\.local\bin\dual-key-remap.exe"
-$shortcut.Save()
