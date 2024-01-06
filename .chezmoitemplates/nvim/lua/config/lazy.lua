997d09b9db3851df7838e059fb0a6af1214eb0c8
Beng Eu <beng@bengeu.com>
2024-01-07 01:44:01 +0800

    fix: remove python-semshi, seems buggy

--- .chezmoitemplates/nvim/lua/config/lazy.lua
+++ .chezmoitemplates/nvim/lua/config/lazy.lua
@@ -24,7 +24,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.python-semshi" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.vscode" },
