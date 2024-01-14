4f7f6bda976d954b7aed0346d3879515a5da7b05
Beng Eu <beng@bengeu.com>
2024-01-14 17:35:36 +0800

    feat: add global hotkeys for Todoist and WezTerm that launch or focus then resend keys

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,3 +1,5 @@
+local wf = hs.window.filter
+
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
@@ -19,19 +21,79 @@ for key, bundle_id_and_args in pairs({
   v = "com.neovide.neovide",
   w = "com.github.wez.wezterm",
 }) do
-  local bundleID = bundle_id_and_args[1] or bundle_id_and_args
+  local bundle_id = bundle_id_and_args[1] or bundle_id_and_args
 
   hs.hotkey.bind({ "ctrl", "option", "shift" }, key, function()
-    local app = hs.application(bundleID)
+    local app = hs.application(bundle_id)
     local args = bundle_id_and_args[2]
 
     if app and app:isFrontmost() then
       app:hide()
     elseif args then
-      os.execute("open -b " .. bundleID .. " " .. args)
+      os.execute("open -b " .. bundle_id .. " " .. args)
     else
-      hs.application.open(bundleID)
+      hs.application.open(bundle_id)
     end
+  end)
+end
+
+for _, mods_and_key in ipairs({
+  {
+    "com.todoist.mac.Todoist",
+    { "ctrl", "cmd" },
+    "a",
+    function(mods, key, hotkey)
+      local timer = hs.timer.doEvery(1, function()
+        hs.eventtap.keyStroke(mods, key)
+      end)
+
+      local wf_todoist
+      wf_todoist = wf.new({ "Todoist" })
+        :subscribe(wf.windowVisible, function(win)
+          if win:size().w == 640 then
+            timer:stop()
+            wf_todoist:unsubscribeAll()
+            hotkey:enable()
+          else
+            win:close()
+          end
+        end)
+    end,
+  },
+  { "com.github.wez.wezterm", { "ctrl", "shift" }, "1" },
+  { "com.github.wez.wezterm", { "ctrl", "shift" }, "2" },
+  { "com.github.wez.wezterm", { "ctrl", "shift" }, "3" },
+  { "com.github.wez.wezterm", { "ctrl", "shift" }, "4" },
+  { "com.github.wez.wezterm", { "ctrl", "shift" }, "5" },
+  { "com.github.wez.wezterm", { "ctrl", "shift" }, "6" },
+  { "com.github.wez.wezterm", { "ctrl", "shift" }, "7" },
+  { "com.github.wez.wezterm", { "ctrl", "shift" }, "8" },
+  { "com.github.wez.wezterm", { "ctrl", "shift" }, "9" },
+}) do
+  local bundle_id = mods_and_key[1]
+  local mods = mods_and_key[2]
+  local key = mods_and_key[3]
+  local after_launch = mods_and_key[4]
+
+  local hotkey
+  hotkey = hs.hotkey.bind(mods, key, function()
+    hotkey:disable()
+
+    local app = hs.application(bundle_id)
+
+    if app then
+      app:setFrontmost()
+    else
+      hs.application.open(bundle_id, 10, true)
+
+      if after_launch then
+        after_launch(mods, key, hotkey)
+        return
+      end
+    end
+
+    hs.eventtap.keyStroke(mods, key)
+    hotkey:enable()
   end)
 end
 
@@ -50,16 +112,5 @@ for key, command in pairs({
     print(output)
   end)
 end
-
-local timer
-timer = hs.timer.doEvery(1, function()
-  local todoistApp = hs.application("com.todoist.mac.Todoist")
-  local todoistWindow = todoistApp and todoistApp:mainWindow()
-
-  if todoistWindow then
-    timer:stop()
-    todoistWindow:close()
-  end
-end)
 
 hs.keycodes.inputSourceChanged(hs.reload)
