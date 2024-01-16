a9ed5a58b555d41b699355c6951107ffc601ac19
Beng Eu <beng@bengeu.com>
2024-01-16 15:15:22 +0800

    feat: format c using clang-format with Google style

--- .chezmoitemplates/nvim/lua/plugins/lsp.lua
+++ .chezmoitemplates/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        c = { "clang_format" },
         clojure = { "zprint" },
         cue = { "cue_fmt" },
         json = { "fixjson", "prettier" },
