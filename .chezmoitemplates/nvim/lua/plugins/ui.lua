212968a20fac044d686960359afb4533901979b7
Beng Eu <beng@bengeu.com>
2024-01-29 21:13:08 +0800

    feat: remove neo-tree git status and buffers

--- .chezmoitemplates/nvim/lua/plugins/ui.lua
+++ .chezmoitemplates/nvim/lua/plugins/ui.lua
@@ -17,6 +17,22 @@ return {
     "folke/edgy.nvim",
     opts = {
       animate = { enabled = false },
+      left = {
+        {
+          title = "Neo-Tree",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "filesystem"
+          end,
+          pinned = true,
+          open = function()
+            vim.api.nvim_input("<esc><space>e")
+          end,
+          size = { height = 0.5 },
+        },
+        { title = "Neotest Summary", ft = "neotest-summary" },
+        "neo-tree",
+      },
     },
   },
   {
