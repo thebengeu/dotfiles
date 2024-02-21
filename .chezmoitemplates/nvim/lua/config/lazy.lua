c0b15fae3133c44f10afc1e155925631074e6157
Beng Eu <beng@bengeu.com>
2024-02-21 18:17:43 +0800

    chore: remove edgy

--- .chezmoitemplates/nvim/lua/config/lazy.lua
+++ .chezmoitemplates/nvim/lua/config/lazy.lua
@@ -15,7 +15,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.dap" },
-    { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.editor.navic" },
