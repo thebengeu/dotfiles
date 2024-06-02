b8a4f51fac0bb1e486034c72581560709757d454
Beng Eu <beng@bengeu.com>
2024-06-01 20:21:23 -0600

    refactor: simplify split_nav.py

--- Library/Application Support/iTerm2/Scripts/AutoLaunch/split_nav.py
+++ Library/Application Support/iTerm2/Scripts/AutoLaunch/split_nav.py
@@ -7,47 +7,47 @@ NavigationDirection = iterm2.NavigationDirection
 
 
 async def main(connection):
+    hjkl_keycodes = [
+        Keycode.ANSI_H,
+        Keycode.ANSI_J,
+        Keycode.ANSI_K,
+        Keycode.ANSI_L,
+    ]
+
     app = await iterm2.async_get_app(connection)
 
-    async with iterm2.KeystrokeMonitor(connection) as mon:
+    async with iterm2.KeystrokeMonitor(connection) as keystroke_monitor:
         while True:
-            keystroke = await mon.async_get()
+            keystroke = await keystroke_monitor.async_get()
             keycode = keystroke.keycode
             modifiers = keystroke.modifiers
 
             current_tab = app.current_window.current_tab
             current_session = current_tab.current_session
 
-            if modifiers == [Modifier.CONTROL]:
-                if keycode in [
-                    Keycode.ANSI_H,
-                    Keycode.ANSI_J,
-                    Keycode.ANSI_K,
-                    Keycode.ANSI_L,
-                ]:
-                    focused_nvim_time = await current_session.async_get_variable(
-                        "user.FOCUSED_NVIM_TIME"
-                    )
+            if modifiers == [Modifier.CONTROL] and keycode in hjkl_keycodes:
+                focused_nvim_time = await current_session.async_get_variable(
+                    "user.FOCUSED_NVIM_TIME"
+                )
 
-                    if focused_nvim_time is None:
-                        match keycode:
-                            case Keycode.ANSI_H:
-                                await current_tab.async_select_pane_in_direction(
-                                    NavigationDirection.LEFT
-                                )
-                            case Keycode.ANSI_J:
-                                await current_tab.async_select_pane_in_direction(
-                                    NavigationDirection.BELOW
-                                )
-                            case Keycode.ANSI_K:
-                                await current_tab.async_select_pane_in_direction(
-                                    NavigationDirection.ABOVE
-                                )
-                            case Keycode.ANSI_L:
-                                await current_tab.async_select_pane_in_direction(
-                                    NavigationDirection.RIGHT
-                                )
-
+                if focused_nvim_time is None:
+                    match keycode:
+                        case Keycode.ANSI_H:
+                            await current_tab.async_select_pane_in_direction(
+                                NavigationDirection.LEFT
+                            )
+                        case Keycode.ANSI_J:
+                            await current_tab.async_select_pane_in_direction(
+                                NavigationDirection.BELOW
+                            )
+                        case Keycode.ANSI_K:
+                            await current_tab.async_select_pane_in_direction(
+                                NavigationDirection.ABOVE
+                            )
+                        case Keycode.ANSI_L:
+                            await current_tab.async_select_pane_in_direction(
+                                NavigationDirection.RIGHT
+                            )
             elif (
                 modifiers == [Modifier.OPTION, Modifier.SHIFT]
                 and keycode == Keycode.ANSI_X
