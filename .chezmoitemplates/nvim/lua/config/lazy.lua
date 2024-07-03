a9778709ee9f5b65f1788bcb55c5a25b71684a56
Beng Eu <beng@bengeu.com>
2024-07-03 21:21:47 +0800

    feat: add Elixir extra

--- .chezmoitemplates/nvim/lua/config/lazy.lua
+++ .chezmoitemplates/nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.elixir" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
