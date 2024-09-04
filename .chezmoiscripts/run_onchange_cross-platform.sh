b9dd68aa436007d5521be8eb42356f5559f64493
Beng Eu <beng@bengeu.com>
2024-09-04 20:16:00 +0800

    fix: install ghorg v1.9.12, v1.9.13 has bug, doesn't pick up private repos

--- .chezmoiscripts/run_onchange_cross-platform.sh
+++ .chezmoiscripts/run_onchange_cross-platform.sh
@@ -9,6 +9,7 @@ gh extension install github/gh-copilot
 gh extension install dlvhdr/gh-dash
 gh extension install seachicken/gh-poi
 
+go install github.com/gabrie30/ghorg@v1.9.12
 go install github.com/nao1215/gup@latest
 
 PIP_REQUIRE_VIRTUALENV=false pip3 install --upgrade --user \
