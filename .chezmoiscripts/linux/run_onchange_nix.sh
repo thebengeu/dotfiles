c58aefe791537c85e7dd333a83d2a71c89fd679b
Beng Eu <beng@bengeu.com>
2024-09-29 10:25:48 +0800

    feat: install K8s related binaries on Linux, set up kubeswitch on Fish

--- .chezmoiscripts/linux/run_onchange_nix.sh
+++ .chezmoiscripts/linux/run_onchange_nix.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 nix profile upgrade '.*'
-nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,dbmate,delta,eksctl,fennel,fzf,hledger,hledger-ui,hledger-web,k6,ledger,less,lf,miller,nethogs,nnn,nushell,peco,pspg,vifm}
+nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,dbmate,delta,eksctl,fennel,fzf,hledger,hledger-ui,hledger-web,k6,k9s,kubectl,kubeswitch,ledger,less,lf,miller,nethogs,nnn,nushell,peco,pspg,vifm}
