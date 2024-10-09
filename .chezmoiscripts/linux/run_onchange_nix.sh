e518c6496bf4de5d11c1315626348cdb0f5063cb
Beng Eu <beng@bengeu.com>
2024-10-09 09:03:02 +0800

    fix: remove repos that don't support Noble, install speedtest using Nix

--- .chezmoiscripts/linux/run_onchange_nix.sh
+++ .chezmoiscripts/linux/run_onchange_nix.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 nix profile upgrade '.*'
-nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,dbmate,delta,eksctl,fennel,fzf,hledger,hledger-ui,hledger-web,k6,k9s,krew,kubectl,kubeswitch,ledger,less,lf,miller,nethogs,nnn,nushell,peco,pspg,vifm}
+nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,dbmate,delta,eksctl,fennel,fzf,hledger,hledger-ui,hledger-web,k6,k9s,krew,kubectl,kubeswitch,ledger,less,lf,miller,nethogs,nnn,nushell,peco,pspg,speedtest-cli,vifm}
 krew install krew
