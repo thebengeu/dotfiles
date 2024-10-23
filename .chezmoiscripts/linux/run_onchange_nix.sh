199118cabae0628ac74818015fbc8537a9bcf540
Beng Eu <beng@bengeu.com>
2024-10-24 07:11:31 +0800

    refactor: install most packages via nix instead of `go install` on Linux

--- .chezmoiscripts/linux/run_onchange_nix.sh
+++ .chezmoiscripts/linux/run_onchange_nix.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 nix profile upgrade '.*'
-nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,bottom,dbmate,delta,dua,dust,eksctl,eza,fennel,fzf,gdu,gitui,hledger,hledger-ui,hledger-web,hyperfine,jless,jwt-cli,k6,k9s,krew,kubectl,kubeswitch,ledger,less,lf,mdcat,miller,miniserve,navi,nethogs,nnn,nushell,onefetch,peco,pspg,sd,speedtest-cli,tealdeer,tere,tokei,topgrade,vifm,xh,yazi}
+nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,bottom,cointop,croc,dbmate,delta,dsq,dua,dust,eksctl,eza,fennel,fzf,gdu,gitui,glow,hledger,hledger-ui,hledger-web,hyperfine,jless,jwt-cli,k6,k9s,krew,kubectl,kubeswitch,lazydocker,lazygit,ledger,less,lf,mdcat,miller,miniserve,navi,nethogs,nnn,nushell,onefetch,peco,pspg,scc,sd,speedtest-cli,shfmt,sq,tealdeer,tere,tokei,todoist,topgrade,usql,vifm,walk,xh,yazi,yj}
 krew install krew
