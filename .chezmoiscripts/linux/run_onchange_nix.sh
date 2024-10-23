8615e63ab9107a82c96221df5a19f7a1097b60d9
Beng Eu <beng@bengeu.com>
2024-10-24 01:21:04 +0800

    refactor: install via cargo only on Windows, install via nix on Linux

--- .chezmoiscripts/linux/run_onchange_nix.sh
+++ .chezmoiscripts/linux/run_onchange_nix.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 nix profile upgrade '.*'
-nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,dbmate,delta,dua,eksctl,fennel,fzf,gdu,gitui,hledger,hledger-ui,hledger-web,jless,k6,k9s,krew,kubectl,kubeswitch,ledger,less,lf,miller,nethogs,nnn,nushell,peco,pspg,speedtest-cli,vifm}
+nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,bottom,dbmate,delta,dua,dust,eksctl,eza,fennel,fzf,gdu,gitui,hledger,hledger-ui,hledger-web,hyperfine,jless,jwt-cli,k6,k9s,krew,kubectl,kubeswitch,ledger,less,lf,mdcat,miller,miniserve,navi,nethogs,nnn,nushell,onefetch,peco,pspg,sd,speedtest-cli,tealdeer,tere,tokei,topgrade,vifm,xh,yazi}
 krew install krew
