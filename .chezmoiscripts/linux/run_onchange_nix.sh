42190b74d3b86274dd69328d33fd2cf56e1652f1
Beng Eu <beng@bengeu.com>
2024-10-24 01:10:49 +0800

    refactor: use nix instead of cargo for more packages on Ubuntu

--- .chezmoiscripts/linux/run_onchange_nix.sh
+++ .chezmoiscripts/linux/run_onchange_nix.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 nix profile upgrade '.*'
-nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,dbmate,delta,dua,eksctl,fennel,fzf,gdu,hledger,hledger-ui,hledger-web,k6,k9s,krew,kubectl,kubeswitch,ledger,less,lf,miller,nethogs,nnn,nushell,peco,pspg,speedtest-cli,vifm}
+nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,dbmate,delta,dua,eksctl,fennel,fzf,gdu,gitui,hledger,hledger-ui,hledger-web,jless,k6,k9s,krew,kubectl,kubeswitch,ledger,less,lf,miller,nethogs,nnn,nushell,peco,pspg,speedtest-cli,vifm}
 krew install krew
