79a8364431570835e27ae250f49cce703267a456
Beng Eu <beng@bengeu.com>
2024-10-26 15:43:11 +0800

    feat: install duf, and btop on Linux

--- .chezmoiscripts/linux/run_onchange_nix.sh
+++ .chezmoiscripts/linux/run_onchange_nix.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 nix profile upgrade '.*'
-nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,bottom,cointop,croc,dbmate,delta,dsq,dua,dust,eksctl,eza,fennel,fzf,gdu,ghorg,gitui,glow,hledger,hledger-ui,hledger-web,hyperfine,jless,jwt-cli,k6,k9s,krew,kubeswitch,lazydocker,lazygit,ledger,less,lf,mdcat,miller,miniserve,navi,nethogs,nnn,nushell,onefetch,peco,pspg,scc,sd,speedtest-cli,shfmt,sq,tealdeer,tere,tokei,todoist,topgrade,usql,vifm,walk,xh,yazi,yj}
+nix profile install nixpkgs#{aws-vault,awscli2,bandwhich,bottom,btop,cointop,croc,dbmate,delta,dsq,dua,duf,dust,eksctl,eza,fennel,fzf,gdu,ghorg,gitui,glow,hledger,hledger-ui,hledger-web,hyperfine,jless,jwt-cli,k6,k9s,krew,kubeswitch,lazydocker,lazygit,ledger,less,lf,mdcat,miller,miniserve,navi,nethogs,nnn,nushell,onefetch,peco,pspg,scc,sd,speedtest-cli,shfmt,sq,tealdeer,tere,tokei,todoist,topgrade,usql,vifm,walk,xh,yazi,yj}
 krew install krew
