e3690dd8ea7e01fc05635b3096997b3474c9f3aa
Beng Eu <beng@bengeu.com>
2025-03-26 23:45:50 +0800

    fix: add docs.google.com to surfingkeys blocklist

--- ignored/surfingkeys.js
+++ ignored/surfingkeys.js
@@ -1,2 +1,6 @@
 // ~/.local/share/chezmoi/ignored/surfingkeys.js
-settings.blocklistPattern = ["app.hex.tech", "mail.google.com"].join("|");
+settings.blocklistPattern = [
+  "app.hex.tech",
+  "docs.google.com",
+  "mail.google.com",
+].join("|");
