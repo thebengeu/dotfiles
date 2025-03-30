a8dead5d75b403426a1b9a614c98a21b5d40bf56
Beng Eu <beng@bengeu.com>
2025-03-30 22:31:09 +0800

    fix: don't hide mouse when typing in neovide

--- .chezmoitemplates/nvim/lua/config/options.lua
+++ .chezmoitemplates/nvim/lua/config/options.lua
@@ -56,7 +56,6 @@ if vim.g.neovide then
   vim.g.neovide_cursor_vfx_mode = "pixiedust"
   vim.g.neovide_cursor_vfx_particle_density = 50
   vim.g.neovide_cursor_vfx_particle_lifetime = 2
-  vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_input_macos_option_key_is_meta = "both"
   vim.g.neovide_transparency = 0.95
   vim.g.neovide_underline_stroke_scale = 2.5
