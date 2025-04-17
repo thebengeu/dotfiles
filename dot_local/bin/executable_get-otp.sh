1f8e1e60d075e691e81badb4d9b31469394d31d0
Beng Eu <beng@bengeu.com>
2025-04-17 11:58:04 +0800

    fix: auto dismiss in get-otp.sh and kill insted of using osascript

--- dot_local/bin/executable_get-otp.sh
+++ dot_local/bin/executable_get-otp.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 pkill -f "single $1"
-osascript -e 'tell app "System Events" to display alert "AWS CLI" message "Touch your YubiKey" as critical' >/dev/null 2>&1 &
+osascript -e 'display dialog "Touch your YubiKey..." with title "AWS CLI" buttons {"OK"} giving up after 60' >/dev/null 2>&1 &
+dialog_pid=$!
 otp=$(ykman oath accounts code --single "$1")
-osascript -e 'tell app "System Events" to tell process "System Events" to click button "OK" of window 1' >/dev/null 2>&1
+kill "${dialog_pid}" 2>/dev/null
 echo "${otp}"
