24fbed5985adc2d4d336fe031322d0d488e6a0db
Beng Eu <beng@bengeu.com>
2025-05-24 20:56:40 +0800

    fix: use regexp for surfingkeys blocklistPattern

--- ignored/surfingkeys.js
+++ ignored/surfingkeys.js
@@ -1,6 +1,4 @@
 // ~/.local/share/chezmoi/ignored/surfingkeys.js
-settings.blocklistPattern = [
-  "app.hex.tech",
-  "docs.google.com",
-  "mail.google.com",
-].join("|");
+settings.blocklistPattern = new RegExp(
+  ["app.hex.tech", "docs.google.com", "mail.google.com"].join("|"),
+);
