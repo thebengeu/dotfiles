2fd64050ac98699e800528bc635b1f0c2772e663
Beng Eu <beng@bengeu.com>
2025-07-24 08:05:46 +0800

    fix: ignore Fastmail for Surfingkeys

--- ignored/surfingkeys.js
+++ ignored/surfingkeys.js
@@ -1,4 +1,9 @@
 // ~/.local/share/chezmoi/ignored/surfingkeys.js
 settings.blocklistPattern = new RegExp(
-  ["app.hex.tech", "docs.google.com", "mail.google.com"].join("|"),
+  [
+    "app.fastmail.com",
+    "app.hex.tech",
+    "docs.google.com",
+    "mail.google.com",
+  ].join("|"),
 );
